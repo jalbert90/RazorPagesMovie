e70d34314af23c73e326c04d1f4321c1899f13ec
jalbert90 <jalbert90@gmail.com>
2021-01-09 11:22:30 -0500

    Scaffolding created

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿// Unused usings removed.
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    #region snippet1
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
